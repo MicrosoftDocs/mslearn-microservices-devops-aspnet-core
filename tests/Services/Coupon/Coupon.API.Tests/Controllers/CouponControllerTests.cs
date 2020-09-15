55b7e08b3774878cb82c4736960f597fa4b429ad
Cam Soper <cam.soper@microsoft.com>
2020-09-15 14:04:38 -0500

    Added coupon test
    Co-authored-by: Scott Addie <scaddie@microsoft.com>

---
+++ tests/Services/Coupon/Coupon.API.Tests/Controllers/CouponControllerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Coupon.API.Tests
+{
+    public class CouponControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}

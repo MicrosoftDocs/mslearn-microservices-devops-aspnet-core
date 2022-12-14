718b52f96e690c3c4a4d849323c7c63a75df7076
ShuCiG <90512692+ShuCiG@users.noreply.github.com>
2022-12-14 15:25:39 +0100

    Update CouponController.cs

--- src/Services/Coupon/Coupon.API/Controllers/CouponController.cs
+++ src/Services/Coupon/Coupon.API/Controllers/CouponController.cs
@@ -55,6 +55,7 @@
             var couponDto = _mapper.Translate(coupon);
 
             // Add LogInformation call
+            _logger.LogInformation("Applying coupon {CouponCode}", code);
 
             return couponDto;
         }
